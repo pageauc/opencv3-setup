a96cb7cffaa72bfc7a1bccfb9cbdc4a5ed321d4d
Claude Pageau <pageauc@gmail.com>
2017-07-20 22:28:47 -0400

    Added make install/config and cleanup (needs full testing)

--- cv32-setup.sh
+++ cv32-setup.sh
@@ -4,35 +4,33 @@
 # Then retry menu pick again or continue to next step
 # version 0.32
 
+#------------------------------------------------------------------------------
 function do_anykey ()
 {
-   echo "------------------------------"
-   echo "  Review messages for Errors"
-   echo "  Exit to Console to Resolve"
-   echo "------------------------------"   
    read -p "m)enu e)xit ?" choice
    case "$choice" in
-      m|M ) echo "Back to Main Menu"
-       ;;
-      e|E ) echo "Install Aborted. Bye"
+      m|M ) echo "Return to Main Menu"
+            ;;
+      e|E ) echo "Install Aborted. Exit Menu"
+            echo "Bye"
             exit 1
-       ;;
+            ;;
         * ) echo "invalid Selection"
-            exit 1
-       ;;
+            ;;
    esac
 }
 
+#------------------------------------------------------------------------------
 function do_rpi_update ()
 {
-   cd ~   
+   cd ~
    # Update Raspbian to Lastest Releases
    echo "Updating Raspbian Please Wait ..."
-   echo "---------------------------------"    
+   echo "---------------------------------"
    sudo apt-get update
    echo "Done Raspbian Update ...."
    echo "Upgrading Raspbian Please Wait ..."
-   echo "---------------------------------"    
+   echo "---------------------------------"
    sudo apt-get upgrade
    echo "Done Raspbian Upgrade ..."
    echo ""
@@ -40,27 +38,27 @@ function do_rpi_update ()
    echo "Menu Pick: OpenCV 3.2.0 Install Build Dependencies and Download Source"
    echo "-----------------------------------------------------------------------"
    echo "If there were Significant Changes then Reboot Recommended"
-   echo ""   
+   echo ""
    read -p "Reboot Now? (y/n)?" choice
-   case "$choice" in 
+   case "$choice" in
      y|Y ) echo "yes"
            echo "Rebooting Now"
            sudo reboot
            ;;
-     n|N ) echo "Back To Main Menu"
+     n|N ) echo "Back to Main Menu"
            ;;
        * ) echo "invalid Selection"
-           exit 1
            ;;
   esac
 }
 
+#------------------------------------------------------------------------------
 function do_cv3_dep ()
 {
    cd ~/
    # Install opencv3 build dependencies
    echo "Installing opencv 3.2.0 build and run dependencies"
-   echo "--------------------------------------------------"  
+   echo "--------------------------------------------------"
    sudo apt-get install -y build-essential cmake pkg-config
    sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
    sudo apt-get install -y libgtk2.0-dev libgstreamer0.10-0-dbg libgstreamer0.10-0 libgstreamer0.10-dev libv4l-0 libv4l-dev
@@ -72,28 +70,29 @@ function do_cv3_dep ()
    sudo apt-get install -y libgtkglext1-dev
    sudo apt-get install -y v4l-utils
    wget https://bootstrap.pypa.io/get-pip.py
-   sudo python get-pip.py   
-   sudo apt-get install -y python2.7-dev   
-   sudo pip install numpy 
+   sudo python get-pip.py
+   sudo apt-get install -y python2.7-dev
+   sudo pip install numpy
    echo "Done Install of Build Essentials and Dependencies ..."
-   echo "-----------------------------------------------------"    
+   echo "-----------------------------------------------------"
    echo "Download and unzip opencv 3.2.0 Source Files"
-   echo "-----------------------------------------------------"    
+   echo "-----------------------------------------------------"
    wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.2.0.zip
    unzip opencv.zip
-   wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.2.0.zip   
-   unzip opencv_contrib.zip   
+   wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.2.0.zip
+   unzip opencv_contrib.zip
    echo "Done Install of Build Essentials, Dependencies and OpenCV 3.2.0 Source."
-   echo "Next Step Select Menu Pick:  OpenCV3 3.2.0 Make, Compile and Install"    
+   echo "Next Step Select Menu Pick:  OpenCV3 3.2.0 Make, Compile and Install"
    do_anykey
 }
 
-function do_cv3_install ()
+#------------------------------------------------------------------------------
+function do_cv3_compile ()
 {
    cd ~
    echo "Running cmake prior to compiling opencv 3.2.0"
    echo "---------------------------------------------"
-   echo "This will take a few minutes ...."   
+   echo "This will take a few minutes ...."
    cd ~/opencv-3.2.0/
    if [ ! -d "build" ]; then
      mkdir build
@@ -105,40 +104,112 @@ function do_cv3_install ()
 	-D INSTALL_PYTHON_EXAMPLES=ON \
 	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.2.0/modules \
 	-D BUILD_EXAMPLES=ON \
-	-D ENABLE_NEON=ON .. 
-    
-    echo "---------------------------------------" 
+	-D ENABLE_NEON=ON ..
+
+    echo "---------------------------------------"
     echo " Review cmake messages above for Errors"
-    echo "---------------------------------------" 
+    echo "---------------------------------------"
     echo "n exits to console"
     read -p "Was cmake successful y/n ?" choice
     case "$choice" in
         y|Y ) echo "Compile of openCV ver 3.2.0 will take approx 3 to 4 hours ...."
               echo "NOTE"
-              echo "If single core Edit this script to change line 125 to remove -j2" 
+              echo "If single core Edit this script to change line 125 to remove -j2"
               echo "----------------------------------------------------------------"
               echo "Once Compile is started go for a nice long walk"
               echo "or Binge watch Game of Thrones or Something Else....."
-              echo ""              
+              echo ""
               echo "Run make -j2  (using 2 of 4 cpu cores)"
-              read -p "Press Enter to Begin Compiling"             
+              read -p "Press Enter to Begin Compiling"
               make -j2
-              echo "--------------------------------------------"  
+              echo "--------------------------------------------"
               echo " Check above for Compile Errors"
-              echo "--------------------------------------------"               
-              echo "If OK Reboot to Complete Install of OpenCV"
+              echo "--------------------------------------------"
               echo "If Errors Please Investigate Problem"
+              echo "If OK Select Menu Pick: make install"
               do_anykey
               ;;
         n|N ) echo "If cmake Failed. Investigate Problem and Try again"
-              exit 1
+              sudo make clean
+              echo "Done make clean"
+              echo "Ready to Try full compile once problem resolved"
+              do_anykey
               ;;
           * ) echo "invalid Selection"
-              do_anykey
               ;;
     esac
 }
 
+#------------------------------------------------------------------------------
+function do_cv3_install ()
+{
+    echo "Perform OpenCV 3.2.0 make install"
+    echo "---------------------------------"
+    sudo make install
+    sudo ldconfig
+    if grep -i "/usr/local/lib" /etc/ld.so.conf.d/opencv.conf
+    then
+      echo "Found Entry /usr/local/bin"
+      echo "In File /etc/ld.so.conf.d/opencv.conf"
+    else
+      echo "Inserting Entry /usr/local/lib"
+      echo "into File /etc/ld.so.conf.d/opencv.conf"
+      sudo echo "/usr/local/lib"  >> /etc/ld.so.conf.d/opencv.conf
+      sudo echo " " >> /etc/ld.so.conf.d/opencv.conf
+      echo "Done Config of /etc/ld.so.conf.d/opencv.conf File"
+    fi
+    sudo ldconfig
+    if grep -i "pkgconfig" /etc/bash.bashrc
+    then
+      echo "Found Entry pkgconfig"
+      echo "In File /etc/ld.so.conf.d/opencv.conf"
+    else
+      echo "Inserting Entry /usr/local/lib"
+      echo "into File /etc/ld.so.conf.d/opencv.conf"
+      sudo echo 'PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig' >> /etc/bash.bashrc
+      sudo echo "export PKG_CONFIG_PATH"  >> /etc/bash.bashrc
+      echo "Done Config of /etc/bash.bashrc File" 
+    fi
+    echo "Reboot to Complete Install of OpenCV 3.2.0"
+    echo ""
+    read -p "Reboot Now? (y/n)?" choice
+    case "$choice" in
+       y|Y ) echo "yes"
+             echo "Rebooting Now"
+             sudo reboot
+             ;;
+       n|N ) echo "Back To Main Menu"
+             ;;
+         * ) echo "invalid Selection"
+             ;;
+   esac
+}
+
+#------------------------------------------------------------------------------
+function do_cv3_cleanup ()
+{
+    echo "------------------------------------------"
+    echo "Remove OpenCV 3.2.0 zip and source Folders"
+    echo ""
+    read -p "Remove Now? (y/n)?" choice
+    case "$choice" in
+       y|Y ) echo "yes"
+             cd ~
+             echo "Removing zip Files"
+             rm open*zip
+             echo "Removing OpenCV 3.2.0 Install Folders"
+             sudo rm -R opencv-3.2.0
+             sudo rm -R opencv_contrib-3.2.0
+             echo "Done Cleanup..."
+             do_anykey
+             ;;
+       n|N ) echo "Back To Main Menu"
+             ;;
+         * ) echo "invalid Selection"
+             ;;
+   esac
+
+}
 
 #------------------------------------------------------------------------------
 function do_about()
@@ -152,16 +223,16 @@ You will be asked to reboot during installation.
 
 Run Menu Pick Selections in order and verify successful completion
 before progressing to next step.  This install is configured for
-a multicore Raspberry Pi make -j2.  Modify this script  at
+a multi core Raspberry Pi make -j2.  Modify this script  at
 approx line 125 and remove -j2 after the make command.
 
 For Additional Details See https://github.com/pageauc/opencv3-setup
 
-Script Steps Based on GitHub Repo 
+Script Steps Based on GitHub Repo
 https://github.com/Tes3awy/OpenCV-3.2.0-Compiling-on-Raspberry-Pi
 
              Good Luck
-            
+
 \
 " 35 70 35
 }
@@ -173,8 +244,10 @@ function do_main_menu ()
   SELECTION=$(whiptail --title "opencv 3.2.0 Install Assist" --menu "Arrow/Enter Selects or Tab Key" 20 70 10 --cancel-button Quit --ok-button Select \
   "a " "Raspbian Jessie Update and Upgrade" \
   "b " "OpenCV 3.2.0 Install Build Dependencies and Download Source" \
-  "c " "OpenCV 3.2.0 cmake, make (compile) and make install" \
-  "d " "About" \
+  "c " "OpenCV 3.2.0 Run cmake and make (compile)" \
+  "d " "OpenCV 3.2.0 Run make install and Configure" \
+  "e " "OpenCV 3.2.0 Remove zip Files and Source Folders" \
+  "f " "About" \
   "q " "Quit Menu Back to Console"  3>&1 1>&2 2>&3)
 
   RET=$?
@@ -184,8 +257,10 @@ function do_main_menu ()
     case "$SELECTION" in
       a\ *) do_rpi_update ;;
       b\ *) do_cv3_dep ;;
-      c\ *) do_cv3_install ;; 
-      d\ *) do_about ;;
+      c\ *) do_cv3_compile ;;
+      d\ *) do_cv3_install ;;
+      e\ *) do_cv3_cleanup ;;
+      f\ *) do_about ;;
       q\ *) echo "After OpenCV 3.2.0 Installation is Complete"
             echo "Reboot to Finalize Install of Opencv"
             echo "Then Test OpenCV"
