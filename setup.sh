f49e4122eeeb155f0b4b5c1ae5fb4cf3993463cf
Claude Pageau <pageauc@gmail.com>
2018-06-03 08:28:21 -0400

    Release .50 Added scripts for 3.2 and 3.3 (Testing)

--- setup.sh
+++ setup.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
 # Convenient opencv3 install script written by Claude Pageau 1-Jul-2016
-ver="0.20"
+ver="0.50"
 DEST_DIR='opencv3-setup'  # Default folder install location
 
 cd ~
 if [ -d "$DEST_DIR" ] ; then
   STATUS="Upgrade"
   echo "Upgrade files"
-else  
+else
   echo "New Install"
   STATUS="New Install"
   mkdir -p $DEST_DIR
   echo "$DEST_DIR Folder Created"
-fi 
+fi
 
 cd $DEST_DIR
 INSTALL_PATH=$( pwd )
 
 # Remember where this script was launched from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "-------------------------------------------------------------"
-echo "      opencv32-setup setup.sh script ver $ver"
-echo "Install or Upgrade opencv3-setup.sh "
-echo "-------------------------------------------------------------"
-echo "1 - Downloading opencv32-setup github repo files"
-echo ""
+echo "
+-------------------------------------------------------------
+      setup.sh script ver $ver
+      Install or Upgrade opencv3
+-------------------------------------------------------------
+1 - Downloading opencv3-setup github repo files"
 
-wget -O cv32-setup.sh https://raw.github.com/pageauc/opencv3-setup/master/cv32-setup.sh
-wget -O setup.sh https://raw.github.com/pageauc/opencv3-setup/master/setup.sh
+wget -O cv32-install-menu.sh https://raw.github.com/pageauc/opencv3-setup/master/cv32-install-menu.sh
+wget -O cv33-install-menu.sh https://raw.github.com/pageauc/opencv3-setup/master/cv33-install-menu.sh
 wget -O Readme.md https://raw.github.com/pageauc/opencv3-setup/master/Readme.md
-  
-echo "Done Download"
-echo "-------------------------------------------------------------"
-echo "2 - Make Required Files Executable"
-echo ""
+
+echo "Done Download
+-------------------------------------------------------------
+2 - Make Required Files Executable"
+
 chmod +x *sh
-echo "Done Permissions"
-echo "-------------------------------------------------------------"
-echo "To Run OpenCV3 Menu Install Script"
-echo ""
-echo "    cd ~/opencv3-setup"
-echo "    ./cv32-setup.sh"
-echo ""
+
+echo "
+Done Permissions
+-------------------------------------------------------------
+To Run Appropiate OpenCV3 Menu Install Script
+
+WARNING -  It is recommended you have a minimum size
+           16GB SD card with at least 5GB free or mounted storage drive
+
+    cd ~/opencv3-setup
+
+    ./cv32-install-menu.sh
+or
+    ./cv33-install-menu.sh"
+
 echo $DEST_DIR "Good Luck Claude ..."
 echo "Bye"
 
